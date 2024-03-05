18a4557a27db2eedcef24ec4d68cb5401bca7868
Rafael <rafeltorress1@gmail.com>
2024-03-04 21:27:21 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 5000,
+    slideToShow: 4,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeesd: 2000
+});
+
